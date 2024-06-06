7bf1e7ec290fe1e081994c78098ddfa5a32a63d2
Nho89 <noa1720@gmail.com>
2024-06-06 11:55:56 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,12 @@ export default function Home() {
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
+
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
+          > 
             By{" "}
             <Image
               src="/vercel.svg"
@@ -36,7 +37,7 @@ export default function Home() {
           width={180}
           height={37}
           priority
-        />
+        />geee
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
