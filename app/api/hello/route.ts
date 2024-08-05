ca5058457d1a19eee079cfdfbf6a40cb87c74fbf
Nho89 <noa1720@gmail.com>
2024-08-06 01:05:51 +0200

    coments next

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"; //creación de respuestas HTTP dentro de API Routes
 
 export async function GET(request:Request) {
     return NextResponse.json({data: "hello"})
