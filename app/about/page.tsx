e83ed8f637ee33f53c50785d9f73a5e12d8a1d98
Nho89 <noa1720@gmail.com>
2024-08-23 13:42:53 +0200

    install motion

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { animate } from "motion"
+import "./page.css"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hola 
+      <div className="box-blue" ></div>
     </main>
   );
 }
